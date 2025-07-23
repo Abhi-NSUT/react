af5a06ca10466802a75ffe93006c8773e7d4f6b9
Abhijeet <jitukhatkarabhijeet123@gmail.com>
2025-07-24 00:04:29 +0530

    redux done

--- 10todocontextlocal/src/components/TodoItem.jsx
+++ 10todocontextlocal/src/components/TodoItem.jsx
@@ -4,7 +4,7 @@ import { useTodo } from '../contexts';
 function TodoItem({ todo }) {
     const[isTodoEditable,setIsTodoEditable]=useState(false)
     const[todoMsg,setTodoMsg]=useState(todo.todo)
-    const{updatedTodo,deleteTodo,toggleComplete}=useTodo()
+    const{updatedTodo,deletedTodo,toggleComplete}=useTodo()
     const editTodo=()=>{
         updatedTodo(todo.id,{...todo,todo:todoMsg})
         setIsTodoEditable(false)
@@ -50,7 +50,7 @@ function TodoItem({ todo }) {
             {/* Delete Todo Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
-                onClick={() => deleteTodo(todo.id)}
+                onClick={() => deletedTodo(todo.id)}
             >
                 ❌
             </button>
